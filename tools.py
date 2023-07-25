fdf052942b38e9d16bcffa91cd6751dd8a5b3902
yth <yuting2727@gmail.com>
2023-07-25 10:45:17 -0700

    add: tools, sample text

--- tools.py
+++ tools.py
@@ -11,3 +11,11 @@ def get_file_contents(filename):
             return f.read().strip()
     except FileNotFoundError:
         print("'%s' file not found" % filename)
+
+def save_string_to_file(file_path, text):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(text)
+        print("String successfully saved to file.")
+    except Exception as e:
+        print(f"Error occurred while saving the string: {e}")
